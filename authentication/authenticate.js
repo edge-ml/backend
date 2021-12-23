05acdf5643b7e725742c2315f1339ddd8b9a9639
KtrauM <murat.kurnaz12@gmail.com>
2021-12-23 20:26:04 +0300

    change authentication logic

--- authentication/authenticate.js
+++ authentication/authenticate.js
@@ -1,59 +1,46 @@
-const config = require("config");
-const request = require("request-promise-native");
-const axios = require("axios").default;
-const Model = require("../models/user").model;
+const config = require('config');
+const jwt = require('jsonwebtoken');
+const Model = require('../models/user').model;
+
+function verifyAccessToken(token) {
+	const payload = jwt.verify(token, config.secret);
+	return payload.id;
+}
 
 module.exports = async (ctx, next) => {
-  const url = ctx.request.url.split("/");
-  try {
-    if (
-      url[2].toLowerCase() === "deviceapi" &&
-      url[3].toLowerCase() !== "deletekey" &&
-      url[3].toLowerCase() !== "switchactive" &&
-      url[3].toLowerCase() !== "getkey" &&
-      url[3].toLowerCase() !== "setkey"
-    ) {
-      return next();
-    }
-  } catch (err) {}
-  try {
-    if (ctx.headers.authorization) {
-      // request sends 'Bearer ' so remove it from token
-      const token = ctx.headers.authorization.replace("Bearer ", "");
-      // call auth server to authenticate with jwt
-      const authRoute = config.auth + "/authenticate";
-      const result = await axios.post(
-        authRoute,
-        {},
-        { headers: { Authorization: ctx.headers.authorization } }
-      );
-      // auth server returns user id, to store with user object
-      // check if we see this user for the first time: do we have this authId already in db?
-      const authId = result.data.userId;
-      const user = await Model.find({ authId });
-      if (!user.length) {
-        // if not, create a new user object
-        const document = new Model({ authId });
-        await document.save();
-      }
-      // only return data associated with this user id
-      // add authId to ctx so we can use it later
-      ctx.state.authId = authId;
-      return next();
-    } else {
-      // no token provided
-      ctx.status = 401;
-      ctx.body = {
-        error: "Please provide a valid JWT token",
-      };
-      return ctx;
-    }
-  } catch (err) {
-    console.log(err);
-    ctx.status = 401;
-    ctx.body = {
-      error: "Unauthorized",
-    };
-    return ctx;
-  }
+	const url = ctx.request.url.split('/');
+	try {
+		if (
+			url[2].toLowerCase() === 'deviceapi'
+      && url[3].toLowerCase() !== 'deletekey'
+      && url[3].toLowerCase() !== 'switchactive'
+      && url[3].toLowerCase() !== 'getkey'
+      && url[3].toLowerCase() !== 'setkey'
+		) {
+			return next();
+		}
+	} catch (err) {
+		console.log(err);
+	}
+	let userId;
+	try {
+		const token = ctx.headers.authorization.split(' ')[1];
+		userId = verifyAccessToken(token);
+	} catch (err) {
+		console.log(err);
+		ctx.status(401);
+		ctx.body = {
+			error: 'Unauthorized',
+		};
+		return ctx;
+	}
+	ctx.state.authId = userId;
+	// check if we see this user for the first time: do we have this authId already in db?
+	const user = await Model.find({ authId: userId });
+	if (!user.length) {
+		// if not, create a new user object
+		const document = new Model({ authId: userId });
+		await document.save();
+	}
+	return next();
 };
