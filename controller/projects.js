7323fb821df90fb075df6747749bab54e3e95cfa
Ömer Erdinç Yağmurlu <omeryagmurlu@gmail.com>
2022-06-27 11:21:32 +0200

    fixes #24

--- controller/projects.js
+++ controller/projects.js
@@ -101,10 +101,7 @@ async function createProject(ctx) {
 async function deleteProjectById(ctx) {
   const { authId } = ctx.state;
   const project = await Project.findOne({
-    $and: [
-      { _id: ctx.params.id },
-      { $or: [{ admin: authId }, { users: authId }] },
-    ],
+    $and: [{ _id: ctx.params.id }, { admin: authId }],
   });
   if (project === undefined) {
     ctx.body = { message: "Cannot delete this project" };
