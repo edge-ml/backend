455ffdd074164e7e5465038733113c46ec4bdc71
KtrauM <murat.kurnaz12@gmail.com>
2022-07-03 21:44:35 +0200

    change portal to exclude upgrade & downgrade

--- routing/routes/checkout.js
+++ routing/routes/checkout.js
@@ -8,6 +8,11 @@ const router = new Router();
 
 const YOUR_DOMAIN = 'http://localhost:3000';
 
+const proProductId = 'prod_LsmoNI2tskuI3R';
+const proPriceId = 'price_1LB1EoAI18ryOlAQ3r3GktSV';
+const proPlusProductId = 'prod_Loys2WqALki7gR';
+const proPlusPriceId = 'price_1L7KuJAI18ryOlAQr9RuO7As';
+
 // TODO double check status codes
 // TODO move this section to auth server to increase security
 // current design allows imitation of customerId and act as another customer
@@ -40,11 +45,45 @@ router.post('/create-checkout-session', KoaBody(), async (ctx) => {
 router.post('/create-customer-portal-session', KoaBody(), async (ctx) => {
   // Authenticate your user.
   // TODO move to auth 
+  const configuration = await stripe.billingPortal.configurations.create({
+    features: {
+      customer_update: {
+        enabled: false,
+      },
+      invoice_history: { enabled: true },
+      payment_method_update: { enabled: true },
+      subscription_cancel: {
+        enabled: true,
+        mode: 'at_period_end',
+        cancellation_reason: {
+          enabled: true,
+          options: [
+            'too_expensive', 
+            'missing_features', 
+            'switched_service', 
+            'unused', 
+            'customer_service', 
+            'too_complex', 
+            'low_quality', 
+            'other'
+          ]
+        },
+      },
+      subscription_pause: { enabled: false },
+    },
+    business_profile: {
+      privacy_policy_url: YOUR_DOMAIN,
+      terms_of_service_url: YOUR_DOMAIN,
+    }
+  })
+  console.log('hey');
+  console.log(configuration);
   const customerId = ctx.request.body.customerId;
   console.log(customerId);
   const session = await stripe.billingPortal.sessions.create({
     customer: customerId,
     return_url: YOUR_DOMAIN,
+    configuration: configuration.id,
   });
   ctx.body = { redirectUrl: session.url };
   ctx.status = 200;
