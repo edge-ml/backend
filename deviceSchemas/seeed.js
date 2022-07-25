355515e5accdbcda108e31549402af70e25c60d1
Xray <37886333+Xraydylan@users.noreply.github.com>
2022-07-26 01:51:53 +0200

    No separator

--- deviceSchemas/seeed.js
+++ deviceSchemas/seeed.js
@@ -42,5 +42,5 @@ exports.device = {
                 }
             ]
     },
-    deviceInfo: { name: "Seeed-XIAO", generation: "1.0.0", maxSampleRate: 30 },
+    deviceInfo: { name: "SeeedXIAO", generation: "1.0.0", maxSampleRate: 30 },
 };
